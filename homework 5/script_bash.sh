a24b4cbd6d54af8341758c94fe108d866a127c59
x <x43872@gmail.com>
2021-05-05 01:51:50 +0300

    Redo homework 5

--- homework 5/script_bash.sh
+++ homework 5/script_bash.sh
@@ -2,18 +2,48 @@
 
 readonly PATH_DIR_OUTPUT="/tmp/parsed_log"
 readonly PATH_DIR_REPO="$(dirname "$0")/../"
-readonly PATH_FILE_LOG="$PATH_DIR_REPO""${1-../misc/access.log}"
-readonly PATH_FILE_OUTPUT="$PATH_DIR_OUTPUT/by_bash"
-readonly TYPES_REQUESTS=('GET' 'POST' 'PUT' 'HEAD' 'DELETE' 'PATCH' 'OPTION')
+readonly TYPES_REQUESTS=('CONNECT' 'DELETE' 'GET' 'HEAD' 'OPTIONS' 'PATCH' 'POST' 'PUT' 'TRACE')
+
+PATH_FILE_LOG="$PATH_DIR_REPO${1-../misc/access.log}"
+IFS='|'
 
 mkdir -p "$PATH_DIR_OUTPUT"
 
-printf 'Общее количество запросов\n' > $PATH_FILE_OUTPUT
-# так и не понял, что происходит на 110201 строке, поэтому решил отфильтровать (а не просто заюзать wc -l)
-IFS='|'
-grep -c -E "] \"(${TYPES_REQUESTS[*]})" "$PATH_FILE_LOG" >> $PATH_FILE_OUTPUT
+path_file_log_filtered="$PATH_DIR_OUTPUT/bash_filtered_log"
+grep -E "^.*\"(${TYPES_REQUESTS[*]})" "$PATH_FILE_LOG" > $path_file_log_filtered
+PATH_FILE_LOG="$path_file_log_filtered"
 
-printf '\nОбщее количество запросов по типу\n' >> $PATH_FILE_OUTPUT
+{
+printf 'Общее количество запросов\n'
+wc -l < "$PATH_FILE_LOG"
+
+printf '\nОбщее количество запросов по типу\n'
 for type in "${TYPES_REQUESTS[@]}"; do
-  printf "$type - %s\n" "$(grep -c "] \"$type" < "$PATH_FILE_LOG")" >> $PATH_FILE_OUTPUT
+  printf "$type - %s\n" "$(grep -c "^.*\"$type" < "$PATH_FILE_LOG")"
 done
+
+printf '\nТоп 10 самых частых запросов\n'
+  grep -oP "^.*?\".*? \K.*?(?= )" "$PATH_FILE_LOG" \
+  | sort \
+  | uniq -c \
+  | sort -nr \
+  | head -10 \
+  | awk '{print $2 " - " $1}'
+
+printf '\nТоп 5 самых больших по размеру запросов, которые завершились клиентской ошибкой\n'
+  grep "^.*\".*\" 4" "$PATH_FILE_LOG" \
+  | awk '{print $10 " " $0}' \
+  | sort -nr \
+  | head -5 \
+  | awk '{print $8 " - " $10 " - " $1 " - " $2}'
+
+printf '\nТоп 5 пользователей по количеству запросов, которые завершились серверной ошибкой\n'
+  grep -oP "^.*?(?= .*?\".*?\" 5)" "$PATH_FILE_LOG" \
+  | sort \
+  | uniq -c \
+  | sort -nr \
+  | head -5 \
+  | awk '{print $2 " - " $1}'
+} > "$PATH_DIR_OUTPUT/by_bash"
+
+rm $path_file_log_filtered
--- homework 5/script_python.py
+++ homework 5/script_python.py
@@ -2,38 +2,61 @@
 
 from pathlib import Path
 import argparse
+import collections
+import json
 
-TYPES_REQUESTS = ('GET', 'POST', 'PUT', 'HEAD', 'DELETE', 'PATCH', 'OPTION')
 PATH_DIR_OUTPUT = Path('/tmp/parsed_log')
-PATH_FILE_OUTPUT = PATH_DIR_OUTPUT / 'by_python'
+TYPES_REQUESTS = ('CONNECT', 'DELETE', 'GET', 'HEAD', 'OPTIONS', 'PATCH', 'POST', 'PUT', 'TRACE')
 
+parser = argparse.ArgumentParser()
+parser.add_argument('--file', default='../misc/access.log')
+parser.add_argument('--json', action='store_true')
+args = parser.parse_args()
+path_file_log = Path(__file__).parents[1] / args.file
 
-def get_amount_requests_by_type(type_request, file):
-    amount = sum(f'] \"{type_request}' in line for line in file)
-    file.seek(0)
-    return amount
+PATH_DIR_OUTPUT.mkdir(exist_ok=True)
+string_output = ''
 
+with open(path_file_log) as file_log:
+    lines_log =\
+        [line for line in [line.split(' ') for line in file_log.readlines()] if line[5][1:] in TYPES_REQUESTS]
 
-def main():
-    parser = argparse.ArgumentParser()
-    parser.add_argument('--file', default='../misc/access.log')
-    args = parser.parse_args()
-    path_file_log = Path(__file__).parents[1] / args.file
+string_output += f'Общее количество запросов\n'
+amount_requests_overall = len(lines_log)
+string_output += str(amount_requests_overall)
+                 
+string_output += '\n\nОбщее количество запросов по типу\n'
+amount_requests_by_type = {type: 0 for type in TYPES_REQUESTS}
+for line in lines_log:
+    amount_requests_by_type[line[5][1:]] += 1
+string_output += '\n'.join([f'{type} - {amount_requests_by_type[type]}' for type in amount_requests_by_type])
 
-    PATH_DIR_OUTPUT.mkdir(exist_ok=True)
-    output_string = ''
+string_output += '\n\nТоп 10 самых частых запросов\n'
+top_by_url = collections.Counter([line[6] for line in lines_log]).most_common(10)
+string_output += '\n'.join([f'{request[0]} - {request[1]}' for request in top_by_url])
 
-    with open(PATH_FILE_OUTPUT, 'w') as file_output:
-        with open(path_file_log) as file_log:
-            output_string += 'Общее количество запросов\n'
-            output_string += f'{sum(get_amount_requests_by_type(tr, file_log) for tr in TYPES_REQUESTS)}\n'
+string_output += '\n\nТоп 5 самых больших по размеру запросов, которые завершились клиентской ошибкой\n'
+lines_log_formatted = [[line[6], line[8], int(line[9]), line[0]] for line in lines_log if line[8].startswith('4')]
+top_by_size_with_client_error = sorted(lines_log_formatted, key=lambda x: (x[2]), reverse=True)[:5]
+string_output += '\n'.join([' - '.join([str(line) for line in line]) for line in top_by_size_with_client_error])
 
-            output_string += '\nОбщее количество запросов по типу\n'
-            for type_request in TYPES_REQUESTS:
-                output_string += f'{type_request} - {get_amount_requests_by_type(type_request, file_log)}\n'
+string_output += '\n\nТоп 5 пользователей по количеству запросов, которые завершились серверной ошибкой\n'
+lines_log_formatted = [line[0] for line in lines_log if line[8].startswith('5')]
+top_by_ip_with_server_error = collections.Counter(lines_log_formatted).most_common(5)
+string_output += '\n'.join([f'{entry[0]} - {entry[1]}' for entry in top_by_ip_with_server_error])
 
-            file_output.write(output_string)
+if args.json:
+    json_output = {
+        'requests_overall': amount_requests_overall,
+        'requests_by_type': amount_requests_by_type,
+        'top_by_url': [{'url': entry[0], 'requests': int(entry[1])} for entry in top_by_url],
+        'top_by_size_with_client_error': [{'url': entry[0], 'code': int(entry[1]), 'size': entry[2], 'ip': entry[3]}
+                                          for entry in top_by_size_with_client_error],
+        'top_by_ip_with_server_error': [{'ip': entry[0], 'requests': entry[1]} for entry in top_by_ip_with_server_error]
+    }
 
+    with open(PATH_DIR_OUTPUT / 'by_python.json', 'w') as file_output:
+        json.dump(json_output, file_output)
 
-if __name__ == '__main__':
-    main()
+with open(PATH_DIR_OUTPUT / 'by_python', 'w') as file_output:
+    file_output.write(string_output + '\n')

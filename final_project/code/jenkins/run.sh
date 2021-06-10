35e7b12dfe31dd6e0e19f1fdbe9f25d47ea5152a
x <x43872@gmail.com>
2021-06-10 21:59:45 +0300

    Add allure report and remove useless jenkins-related scripts

--- final_project/code/jenkins/run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-JJB_DIR="$(dirname "$0")/jjb"
-jenkins-jobs --conf "$JJB_DIR/auth.ini" update "$JJB_DIR/job.yml"
-
-curl -X POST -L --user admin:11c76634b216e960e7286fdbb5a84f7e6c http://0.0.0.0:8080/job/myapp_testing/build
